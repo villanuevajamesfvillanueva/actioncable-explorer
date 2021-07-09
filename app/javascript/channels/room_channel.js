b8b5c739c10c7d25ce18fe73795bcc80f17fcebb
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-10 00:39:10 +0800

    added channel room

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,23 @@
+import consumer from "./consumer"
+
+document.addEventListener('turbolinks:load', () => {
+    const element = document.getElementById('room-id');
+    const room_id = element.getAttribute('data-room-id');
+
+    consumer.subscriptions.create({channel: "RoomChannel", room_id: room_id}, {
+        connected() {
+          // Called when the subscription is ready for use on the server
+          console.log("Connected to room channel " + room_id);
+        },
+      
+        disconnected() {
+          // Called when the subscription has been terminated by the server
+        },
+      
+        received(data) {
+          // Called when there's incoming data on the websocket for this channel
+          console.log(data)
+        }
+      });
+});
+
